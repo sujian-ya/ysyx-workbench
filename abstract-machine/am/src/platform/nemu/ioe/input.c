f5b2fa8c2b32d029273df5302b6c800172a33b7d
ysyx_25040105-sheep <sheep325034@gmail.com>
2025-09-15 16:23:04 +0800

    实现IOE(2): 实现键盘外设

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,10 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  // kbd->keydown = 0;
+  // kbd->keycode = AM_KEY_NONE;
+  uint32_t keyboard = inl(KBD_ADDR);
+  // 最高位获取 keydown 状态，低位获取 keycode
+  kbd->keydown = (keyboard & KEYDOWN_MASK) ? true : false;
+  kbd->keycode = keyboard & (~KEYDOWN_MASK);
 }
--- nemu/src/monitor/dtrace.c
+++ nemu/src/monitor/dtrace.c
@@ -9,7 +9,7 @@ void log_dtrace(bool is_write, paddr_t addr, int len, word_t data, const char *m
             addr, len, data, map_name);
     } else {
         log_write("[DTRACE] READ: addr = 0x%08x, len = %d, data = 0x%08x, device = %s\n",
-                addr, len, data, map_name);
+            addr, len, data, map_name);
     }
 #endif
 }
