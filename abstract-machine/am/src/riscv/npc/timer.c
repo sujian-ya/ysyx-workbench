c900048a341d6308b4737523065fe35af53490d2
ysyx_25040105-sheep <sheep325034@gmail.com>
2025-09-25 22:11:18 +0800

    npc的计时器添加完毕

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,10 +1,22 @@
 #include <am.h>
+#include <npc.h>
 
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  // 读取RTC寄存器的值，假设RTC寄存器的低32位在RTC_ADDR，高32位在RTC_ADDR + 4
+  // 为了保证读取的一致性，先读高32位，再读低32位，然后再读一次高32位进行验证
+  // 如果两次读到的高32位不一样，说明在读低32位时高32位发生了变化，需要重新读
+  // 直到两次读到的高32位一样为止
+  // 最终将高32位和低32位合并成一个64位的值，表示微秒数
+  uint32_t hi, lo, hi2;
+  do {
+    hi = inl(RTC_ADDR + 4);
+    lo = inl(RTC_ADDR);
+    hi2 = inl(RTC_ADDR + 4);
+  } while (hi != hi2);
+  uptime->us = ((uint64_t)hi << 32) | lo;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
