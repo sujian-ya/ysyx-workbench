746d41ee5a4027575ceb8fa75b5b26eb463a5ccb
231220000-Zhang San <zhangsan@foo.com>
2025-09-28 11:27:11 +0800

    npc的gpu设备模块添加完成

---
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -0,0 +1,66 @@
+#include <am.h>
+#include <npc.h>
+
+#define SYNC_ADDR (VGACTL_ADDR + 4)
+
+void __am_gpu_init() {
+  // uint32_t vga_ctl = inl(VGACTL_ADDR);
+  // // 宽度和高度分别存储在高16位和低16位
+  // int w = (vga_ctl >> 16) & 0xffff;
+  // int h = vga_ctl & 0xffff;
+  // // 定义一个指向显存起始地址的指针，并将其初始化为FB_ADDR
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (int i = 0; i < w * h; i++) {
+  //   fb[i] = i;
+  // }
+  // outl(SYNC_ADDR, 1);
+}
+
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  // 再次从VGA控制寄存器读取屏幕宽度和高度，以及计算显存大小
+  uint32_t vga_ctl = inl(VGACTL_ADDR);
+  int w = (vga_ctl >> 16) & 0xffff;
+  int h = vga_ctl & 0xffff;
+  size_t vmem_size = w * h * sizeof(uint32_t);
+
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = w, .height = h,
+    .vmemsz = vmem_size
+  };
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t vga_ctl = inl(VGACTL_ADDR);
+  // 宽度和高度分别存储在高16位和低16位
+  int screen_w = (vga_ctl >> 16) & 0xffff;
+  int screen_h = vga_ctl & 0xffff;
+  // 获取帧缓冲区和像素数据的指针
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t*)ctl->pixels;
+  int draw_x = ctl->x, draw_y = ctl->y, draw_w = ctl->w, draw_h = ctl->h;
+
+  // 优化边界检查，避免在循环内部频繁判断
+  int start_x = (draw_x < 0) ? 0 : draw_x;
+  int end_x = (draw_x + draw_w > screen_w) ? screen_w : draw_x + draw_w;
+  int start_y = (draw_y < 0) ? 0 : draw_y;
+  int end_y = (draw_y + draw_h > screen_h) ? screen_h : draw_y + draw_h;
+
+  for (int j = start_y; j < end_y; j++) {
+    for (int i = start_x; i < end_x; i++) {
+      // 计算源像素的相对位置
+      int src_x = i - draw_x;
+      int src_y = j - draw_y;
+      
+      fb[j * screen_w + i] = pixels[src_y * draw_w + src_x];
+    }
+  }
+
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
--- npc/csrc/device/device.cpp
+++ npc/csrc/device/device.cpp
@@ -8,15 +8,15 @@
 void init_map();
 void init_serial();
 void init_timer();
-// void init_vga();
+void init_vga();
 void init_i8042();
 // void init_audio();
 // void init_disk();
 // void init_sdcard();
 // void init_alarm();
 
-// void send_key(uint8_t, bool);
-// void vga_update_screen();
+void send_key(uint8_t, bool);
+void vga_update_screen();
 
 void device_update() {
   static uint64_t last = 0;
@@ -26,37 +26,37 @@ void device_update() {
   }
   last = now;
 
-  // IFDEF(CONFIG_HAS_VGA, vga_update_screen());
+  IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 
-// #ifndef CONFIG_TARGET_AM
-//   SDL_Event event;
-//   while (SDL_PollEvent(&event)) {
-//     switch (event.type) {
-//       case SDL_QUIT:
-//         npc_state.state = NPC_QUIT;
-//         break;
-// #ifdef CONFIG_HAS_KEYBOARD
-//       // If a key was pressed
-//       case SDL_KEYDOWN:
-//       case SDL_KEYUP: {
-//         uint8_t k = event.key.keysym.scancode;
-//         bool is_keydown = (event.key.type == SDL_KEYDOWN);
-//         send_key(k, is_keydown);
-//         break;
-//       }
-// #endif
-//       default: break;
-//     }
-//   }
-// #endif
+#ifndef CONFIG_TARGET_AM
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        npc_state.state = NPC_QUIT;
+        break;
+#ifdef CONFIG_HAS_KEYBOARD
+      // If a key was pressed
+      case SDL_KEYDOWN:
+      case SDL_KEYUP: {
+        uint8_t k = event.key.keysym.scancode;
+        bool is_keydown = (event.key.type == SDL_KEYDOWN);
+        send_key(k, is_keydown);
+        break;
+      }
+#endif
+      default: break;
+    }
+  }
+#endif
 }
 
-// void sdl_clear_event_queue() {
-// #ifndef CONFIG_TARGET_AM
-//   SDL_Event event;
-//   while (SDL_PollEvent(&event));
-// #endif
-// }
+void sdl_clear_event_queue() {
+#ifndef CONFIG_TARGET_AM
+  SDL_Event event;
+  while (SDL_PollEvent(&event));
+#endif
+}
 
 void init_device() {
   IFDEF(CONFIG_TARGET_AM, ioe_init());
@@ -64,7 +64,7 @@ void init_device() {
 
   IFDEF(CONFIG_HAS_SERIAL, init_serial());
   IFDEF(CONFIG_HAS_TIMER, init_timer());
-  // IFDEF(CONFIG_HAS_VGA, init_vga());
+  IFDEF(CONFIG_HAS_VGA, init_vga());
   IFDEF(CONFIG_HAS_KEYBOARD, init_i8042());
   // IFDEF(CONFIG_HAS_AUDIO, init_audio());
   // IFDEF(CONFIG_HAS_DISK, init_disk());
